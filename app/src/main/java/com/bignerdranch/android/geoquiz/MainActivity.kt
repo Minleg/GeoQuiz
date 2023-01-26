d67e67c1213ce96d662bedbde859a85e7c0424f0
minleg <minleg@minleg.com>
2023-01-26 11:39:09 -0600

    previous button added

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -12,8 +12,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
+    private lateinit var prevButton: Button
 
-    // create a list of question objects
+    // create a list of question objects, each question object with a single question and the answer for the question
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
         Question(R.string.question_oceans, true),
@@ -33,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
+        prevButton = findViewById(R.id.prev_button)
 
         trueButton.setOnClickListener {
             checkAnswer(true)
@@ -47,24 +49,42 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        // next question can be loaded by clicking the question text view
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        // if previous button is clicked
+        prevButton.setOnClickListener {
+            if (currentIndex == 0) { // if index is 0, point to the last question in the list
+                currentIndex = questionBank.size - 1
+            } else { // point to the previous question otherwise
+                currentIndex -= 1
+            }
+            updateQuestion()
+        }
+
         // puts the first question in the text view
         updateQuestion()
     }
 
-    private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
-        questionTextView.setText(questionTextResId)
+    private fun updateQuestion() { // gets the question at the index
+        val questionTextResId =
+            questionBank[currentIndex].textResId // the resource id for the question
+        questionTextView.setText(questionTextResId) // sets the question textview with the question on the index
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        /* Checks the answer stored in the list with the user's answer */
+        val correctAnswer = questionBank[currentIndex].answer // gets the answer for the question in the list
 
-        val messageResId = if ( userAnswer == correctAnswer) {
-            R.string.correct_toast
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast // correct answer toast is set
         } else {
             R.string.incorrect_toast
         }
 
-        Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show() // shows the user , toast with message whether they are correct or not
     }
 }
